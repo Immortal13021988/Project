34661963554ac593f592ba6b0da3b5b1706450f3
iua.suranov <pradedura@mail.ru>
2025-05-17 21:54:54 +0300

    fix masks.py test_masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,19 @@
-def get_mask_card_number(card_number: str) -> str:
+from typing import Union
+
+
+def get_mask_card_number(card_number: Union[str, int]) -> str:
     """Функция маскировки номера банковской карты"""
     card_number = str(card_number)
-    mask_card_number = f'{"".join(card_number[:4])} {"".join(card_number[4:6])}** **** {"".join(card_number[12:])}'
-    return mask_card_number
+    if len(card_number) == 16 and card_number.isdigit():
+        mask_card_number = f'{"".join(card_number[:4])} {"".join(card_number[4:6])}** **** {"".join(card_number[12:])}'
+        return mask_card_number
+    return "Номер карты введен неверно"
 
 
-def get_mask_account(score: str) -> str:
+def get_mask_account(score: Union[str, int]) -> str:
     """Функция маскировки номера банковского счета"""
     score = str(score)
-    mask_card_number = f'**{"".join(score[-4:])}'
-    return mask_card_number
+    if len(score) == 20 and score.isdigit():
+        mask_card_number = f'**{"".join(score[-4:])}'
+        return mask_card_number
+    return "Номер счета введен неверно"
