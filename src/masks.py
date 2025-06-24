0e559b85169bb4b0a120bc3ef18eaeba0a718719
iua.suranov <pradedura@mail.ru>
2025-06-25 02:37:11 +0300

    add logger in two files

--- src/masks.py
+++ src/masks.py
@@ -1,19 +1,35 @@
 from typing import Union
+import logging
+
+logger = logging.getLogger('masks')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('logs/masks.log')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def get_mask_card_number(card_number: Union[str, int]) -> str:
     """Функция маскировки номера банковской карты"""
+    logger.info(f'Функция маскировки номера банковской карты запущена')
     card_number = str(card_number)
+    logger.info(f'Проверяем корректность номера банковской карты')
     if len(card_number) == 16 and card_number.isdigit():
         mask_card_number = f'{"".join(card_number[:4])} {"".join(card_number[4:6])}** **** {"".join(card_number[12:])}'
+        logger.info(f'Маскировки номера банковской карты выполнена')
         return mask_card_number
+    logger.error(f'Провалена проверка корректности номера банковской карты')
     return "Номер карты введен неверно"
 
 
 def get_mask_account(score: Union[str, int]) -> str:
     """Функция маскировки номера банковского счета"""
+    logger.info(f'Функция маскировки номера счета запущена')
     score = str(score)
+    logger.info(f'Проверяем корректность номера счета')
     if len(score) == 20 and score.isdigit():
         mask_card_number = f'**{"".join(score[-4:])}'
+        logger.info(f'Маскировки номера счета выполнена')
         return mask_card_number
+    logger.error(f'Провалена проверка корректности номера счета')
     return "Номер счета введен неверно"
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,13 @@
 import json
 from typing import Any
+import logging
+
+logger = logging.getLogger('utils')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('logs/utils.log')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def open_json(file_path: str) -> list[Any]:
@@ -8,11 +16,16 @@ def open_json(file_path: str) -> list[Any]:
     с данными о финансовых транзакциях
     """
     try:
+        logger.info(f'Выполняем открытия JSON-файла')
         with open(file_path, encoding="utf-8") as json_file:
             trans = json.load(json_file)
+        logger.info(f'Проверяем соответствие содержимого файла')
         if type(trans) is list:
+            logger.info(f'Проверка файла прошла успешно')
             return trans
         else:
+            logger.error(f'Проверка файла провалена (содержимое файла не соответствует)')
             return []
-    except (json.JSONDecodeError, FileNotFoundError):
+    except (json.JSONDecodeError, FileNotFoundError) as ex:
+        logger.error(f'Произошла ошибка: {ex}')
         return []
