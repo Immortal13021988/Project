f4a750bba92ec389693f9312daee3030622c2df8
iua.suranov <pradedura@mail.ru>
2025-06-24 17:25:40 +0300

    fix utils.py

--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,9 @@ def open_json(file_path: str) -> list[Any]:
     try:
         with open(file_path, encoding="utf-8") as json_file:
             trans = json.load(json_file)
-        return trans
-    except Exception:
+        if type(trans) is list:
+            return trans
+        else:
+            return []
+    except (json.JSONDecodeError, FileNotFoundError):
         return []
