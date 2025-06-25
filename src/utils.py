58bbb4bf57f9add013add9d38c5d71aea3e2a1b0
iua.suranov <pradedura@mail.ru>
2025-06-25 11:00:23 +0300

    fix logger in two files

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,10 @@ import json
 from typing import Any
 import logging
 
-logger = logging.getLogger('utils')
-logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('logs/utils.log', 'w')
-file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: - %(message)s')
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("logs/utils.log", "w")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: - %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
@@ -16,16 +16,16 @@ def open_json(file_path: str) -> list[Any]:
     с данными о финансовых транзакциях
     """
     try:
-        logger.info(f'Выполняем открытия JSON-файла')
+        logger.info("Выполняем открытия JSON-файла")
         with open(file_path, encoding="utf-8") as json_file:
             trans = json.load(json_file)
-        logger.info(f'Проверяем соответствие содержимого файла')
+        logger.info("Проверяем соответствие содержимого файла")
         if type(trans) is list:
-            logger.info(f'Проверка файла прошла успешно')
+            logger.info("Проверка файла прошла успешно")
             return trans
         else:
-            logger.error(f'Проверка файла провалена (содержимое файла не соответствует)')
+            logger.error("Проверка файла провалена (содержимое файла не соответствует)")
             return []
     except (json.JSONDecodeError, FileNotFoundError) as ex:
-        logger.error(f'Произошла ошибка: {ex}')
+        logger.error(f"Произошла ошибка: {ex}")
         return []
