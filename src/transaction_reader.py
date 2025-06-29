ec65eb648bb41e9a2fee5fa85e689369c4751502
iua.suranov <pradedura@mail.ru>
2025-06-29 03:04:18 +0300

    fix three files

--- src/transaction_reader.py
+++ src/transaction_reader.py
@@ -1,31 +1,26 @@
 import csv
-from typing import Any
 import pandas as pd
 
 
-def open_csv(file_path: str) -> list[Any]:
+def open_csv(file_path: str) -> (list, str):
     """
     Функция, которая принимает на вход путь до CSV-файла и возвращает список словарей
     с данными о финансовых транзакциях
     """
-    list_trans = []
     try:
-        with open(file_path, encoding="utf-8") as csv_file:
-            trans = csv.DictReader(csv_file, delimiter=";")
-            for row in trans:
-                list_trans.append(row)
-        return list_trans
+        df = pd.read_csv(file_path, delimiter=";")
+        return df.to_dict('records')
     except (Exception, FileNotFoundError):
-        return list_trans
+        return []
 
 
 def open_xlsx(file_path: str) -> (pd.DataFrame, str):
     """
-        Функция, которая принимает на вход путь до CSV-файла и возвращает список словарей
+        Функция, которая принимает на вход путь до XLSX-файла и возвращает список словарей
         с данными о финансовых транзакциях
     """
     try:
         df = pd.read_excel(file_path)
-        return df
-    except (Exception, FileNotFoundError) as ex:
-        return f'Ошибка: {ex}'
+        return df.to_dict('records')
+    except (Exception, FileNotFoundError):
+        return []
