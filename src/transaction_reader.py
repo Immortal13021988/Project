a16c9c667505debad4f8ed39b7e8ff9f56f02331
iua.suranov <pradedura@mail.ru>
2025-06-29 03:19:27 +0300

    fix four files (linters)

--- src/transaction_reader.py
+++ src/transaction_reader.py
@@ -1,26 +1,25 @@
-import csv
 import pandas as pd
 
 
-def open_csv(file_path: str) -> (list, str):
+def open_csv(file_path: str) -> list:
     """
     Функция, которая принимает на вход путь до CSV-файла и возвращает список словарей
     с данными о финансовых транзакциях
     """
     try:
         df = pd.read_csv(file_path, delimiter=";")
-        return df.to_dict('records')
+        return df.to_dict("records")
     except (Exception, FileNotFoundError):
         return []
 
 
-def open_xlsx(file_path: str) -> (pd.DataFrame, str):
+def open_xlsx(file_path: str) -> list:
     """
-        Функция, которая принимает на вход путь до XLSX-файла и возвращает список словарей
-        с данными о финансовых транзакциях
+    Функция, которая принимает на вход путь до XLSX-файла и возвращает список словарей
+    с данными о финансовых транзакциях
     """
     try:
         df = pd.read_excel(file_path)
-        return df.to_dict('records')
+        return df.to_dict("records")
     except (Exception, FileNotFoundError):
         return []
